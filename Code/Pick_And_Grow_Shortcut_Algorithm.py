d7b907a4fe2784c72a58c751bc1a92e3fc228561
sa05169 <49056370+sa05169@users.noreply.github.com>
2019-04-21 16:51:07 +0500

    Pointer-type Dictionary representation of routes

--- Code/Pick_And_Grow_Shortcut_Algorithm.py
+++ Code/Pick_And_Grow_Shortcut_Algorithm.py
@@ -1,125 +1,162 @@
 import math
+import time
+
+def list_representation_route(mid_index):
+    global route, n, complexity
+    complexity = complexity + 2*(n-4)
+    trace_ref = [mid_index]
+    while type(route[trace_ref[-1]][1]) is dict:
+        trace_ref.append(route[trace_ref[-1]][1]['sub_path_ref'])
+    min_route = route[trace_ref[-1]][1]
+    trace_ref.pop()
+    while trace_ref != []:
+        connection = route[trace_ref[-1]][1]['connection']
+        if connection[0] == 'append':
+            min_route.append(connection[1])
+        else:
+            min_route = [connection[1]] + min_route
+        trace_ref.pop(-1)
+    return min_route
+
+def element_routes(cities):
+    global distance, complexity
+    ans = dict()
+    k = cities['start']
+    i = cities[k]
+    while i != 'finish':
+        cities[k] = cities[i]
+        l = 'start'
+        j = cities[l]
+        while j != cities[i]:
+            cities[l] = cities[j]
+            q = cities['start']
+            while q != 'finish':
+                complexity = complexity + 1
+                ans[(pair(i,j),q)] = [distance[pair(i,q)]+distance[pair(j,q)], [i,q,j], -1]
+                q = cities[q]
+            cities[l] = j
+            l = j
+            j = cities[l]
+        cities[k] = i
+        k = i
+        i = cities[k]
+    return ans
 
 def circular_route_tsp(cities):
+    start = time.time()
     global distance, n, route, complexity
     min_len=-1
     min_route=[]
-    p=len(cities)
-    complexity = complexity + (p-2)*(p-1)//2
-    for i in range(2,len(cities)):
-        for j in range(1,i):
-            iroute = one_way_tsp(cities[j], cities[i], cities[1:j] + cities[j+1:i] + cities[i+1:])
-            iroute[0] = iroute[0] + distance[pair(cities[0], cities[i])] + distance[pair(cities[0], cities[j])]
-            iroute[1].append(cities[0])
+    mid = cities.copy()
+    ref_city = cities['start']
+    cities['start'] = cities[cities['start']]
+    k = cities['start']
+    i = cities[k]
+    mid_index = -1
+    while i != 'finish':
+        cities[k] = cities[i]
+        l = 'start'
+        j = cities[l]
+        while j != cities[i]:
+            cities[l] = cities[j]
+            iroute = one_way_tsp(j, i, cities)
+            iroute[0] = iroute[0] + distance[pair(ref_city, i)] + distance[pair(ref_city, j)]
             if iroute[0] < min_len or min_len == -1:
                 min_len = iroute[0]
                 min_route = iroute[1]
-    return [min_len, min_route]
-            
+                mid_index = iroute[2]
+            cities[l] = j
+            l = j
+            j = cities[l]
+        cities[k] = i
+        k = i
+        i = cities[k]
+    print('time taken:',time.time()-start)
+    min_route = [ref_city]+list_representation_route(mid_index)
+    return [min_len, min_route]            
 
 def one_way_tsp(start, end, cities, ipath = -1):
-    global distance, route, complexity
-    if len(cities) == 1:
-        return [distance[pair(start, cities[0])] + distance[pair(cities[0], end)],
-                [start, cities[0], end], -1]
+    global distance, route, complexity, len3_routes
+    if cities[cities['start']]=='finish':
+        complexity = complexity + 1
+        return len3_routes[(pair(start,end), cities['start'])]
     if ipath == -1:
         ipath = path_index(start, end, cities)
-    if ipath in route.keys():
+    if ipath in route.keys(): # has complexity O(1)
+        complexity = complexity + 1
         return [route[ipath][0], route[ipath][1], ipath]
     min_len = -1
-    complexity = complexity + len(cities)
-    for i in range(len(cities)):
-        index = path_index(start, cities[i], cities[:i] + cities[i+1:])
+    p=0
+    i='start'
+    while cities[i]!='finish':
+        p=p+1
+        i = cities[i]
+    i = cities['start']
+    j = 'start'
+    while i != 'finish':
+        cities[j] = cities[i]
+        index = path_index(start, i, cities)
         if index in route.keys():
-            ilen = route[index][0] + distance[pair(cities[i], end)]
+            complexity = complexity + 1
+            ilen = route[index][0] + distance[pair(i, end)]
             if ilen < min_len or min_len == -1:
                 min_len = ilen
-                if route[index][1][0] == start:
-                    min_route = route[index][1].copy()
-                    min_route.append(end)
+                if p == 2:
+                    if route[index][1][0] == start:
+                        min_route = route[index][1].copy()
+                        min_route.append(end)
+                    else:
+                        min_route=[end] + route[index][1]
+                elif p == 3:
+                    if route[index][1][0] == start:
+                        min_route = {'sub_path_ref':index,'connection':('append',end),'start':start,'end':end}
+                    else:
+                        min_route = {'sub_path_ref':index,'connection':('header',end),'start':end,'end':start}
                 else:
-                    min_route=[end] + route[index][1]
+                    if route[index][1]['start'] == start:
+                        min_route = {'sub_path_ref':index,'connection':('append',end),'start':start,'end':end}
+                    else:
+                        min_route = {'sub_path_ref':index,'connection':('header',end),'start':end,'end':start}
         else:
-            recursive_call = one_way_tsp(start, cities[i], cities[:i] + cities[i+1:], index)
-            ilen = recursive_call[0] + distance[pair(cities[i], end)]
+            recursive_call = one_way_tsp(start, i, cities, index)
+            ilen = recursive_call[0] + distance[pair(i, end)]
             if ilen < min_len or min_len == -1:
                 min_len = ilen
-                if recursive_call[1][0] == start:
-                    min_route = recursive_call[1].copy()
-                    min_route.append(end)
+                if p == 2:
+                    if recursive_call[1][0] == start:
+                        min_route = recursive_call[1].copy()
+                        min_route.append(end)
+                    else:
+                        min_route = [end] + recursive_call[1]
+                elif p == 3:
+                    if recursive_call[1][0] == start:
+                        min_route = {'sub_path_ref':index,'connection':('append',end),'start':start,'end':end}
+                    else:
+                        min_route = {'sub_path_ref':index,'connection':('header',end),'start':end,'end':start}
                 else:
-                    min_route = [end] + recursive_call[1]
+                    if recursive_call[1]['start'] == start:
+                        min_route = {'sub_path_ref':index,'connection':('append',end),'start':start,'end':end}
+                    else:
+                        min_route = {'sub_path_ref':index,'connection':('header',end),'start':end,'end':start}
+        cities[j] = i
+        j = i
+        i = cities[i]
     route[ipath] = [min_len, min_route]
-    '''if min_route[0] == start:
-        for i in range(1,len(min_route)-4):
-            for j in range(i+4,len(min_route)):
-                route[path_index(min_route[i],min_route[j],min_route[i+1:j])] = [sum([distance[pair(min_route[k],min_route[k+1])] for k in range(len(min_route[i:j+1])-1)]), min_route[i:j+1]]
-    else:
-        for i in range(len(min_route)-5):
-            for j in range(i+4,len(min_route)-1):
-                route[path_index(min_route[i],min_route[j],min_route[i+1:j])] = [sum([distance[pair(min_route[k],min_route[k+1])] for k in range(len(min_route[i:j+1])-1)]), min_route[i:j+1]]
-    '''
     return [min_len, min_route, ipath]
 
-def get_shortcuts(cities):
-    global distance, n, route, complexity
-    min_len=-1
-    min_route=[]
-    p=len(cities)
-    complexity = complexity + p*(p-1)*(p-2)//2
-    for i in range(1,len(cities)):
-        for j in range(i):
-            end = cities[i]
-            start = cities[j]
-            mid_cities =  [k for k in cities if k != end and k != start]
-            if start in mid_cities or end in mid_cities:
-                print(mid_cities,cities,end,start,i,j)
-            iroute = one_way_tsp(start, end, mid_cities,-1)
-            min_len = iroute[0]
-            min_route = iroute[1]
-            mid_cities = min_route[1:len(min_route)-1]
-            ipath = iroute[2]
-            if end == min_route[0]:
-                for a in range(len(cities)-5):
-                    for b in range(a+4,len(cities)-1):
-                        index = path_index(min_route[a], min_route[b], min_route[a+1:b])
-                        if index not in route.keys():
-                            route[index] = [0,0]
-                            route[index][1] = min_route[a:b+1]
-                            route[index][0] = sum([distance[pair(route[index][1][p],route[index][1][p+1])] for p in range(len(route[index][1])-1)])#route[ipath][0] - route[path_index(start,min_route[b], min_route[b+1:len(min_route)-1])][0]
-            else:
-                for b in range(len(cities)-1,4,-1):
-                    for a in range(1,b-3):
-                        index = path_index(min_route[a], min_route[b], min_route[a+1:b])
-                        if index not in route.keys():
-                            route[index] = [0,0]
-                            route[index][1] = min_route[a:b+1]
-                            route[index][0] = sum([distance[pair(route[index][1][p],route[index][1][p+1])] for p in range(len(route[index][1])-1)])#route[ipath][0] - route[path_index(start,min_route[b], min_route[b+1:len(min_route)-1])][0]
-    return len(route.keys())
-
-def path_index(start,end,cities):
+def path_index(start,end,cities):# cities are always sorted
     global n, complexity
-    if len(cities) < 2:
+    if cities[cities['start']]=='finish':
         return -1
-    #complexity = complexity + len(cities)
-    cities.sort()
-    node = [start,end,cities[0],cities[1]]
-    node.sort()
     if start > end:
         start, end = end, start
-    count=0
-    for i in cities[2:]:
-        if i > node[3]:
-            count = count + 2**(i-5)
-        elif i > node[2]:
-            count = count + 2**(i-4)
-        elif i > node[1]:
-            count = count + 2**(i-3)
-        elif i > node[0]:   
-            count = count + 2**(i-2)
-        else:
-            count = count + 2**(i-1)
-    return (start-1+(end-1)*(end-2)//2,cities[0]-1+(cities[1]-1)*(cities[1]-2)//2,count)
+    i = cities[cities[cities['start']]]
+    k = 0
+    while i != 'finish':
+        complexity +=1
+        k += 2**i
+        i = cities[i]
+    return (start-1+(end-1)*(end-2)//2,cities['start']-1+(cities[cities['start']]-1)*(cities[cities['start']]-2)//2,k)
 
 def all_dis(lst):
     ans=[]
@@ -148,27 +185,27 @@ def complexity_function(n): # Note: there is no worst case or best case as the n
         t_complexity = t_complexity + iset*(i-1)
     m_complexity = m_complexity + 1
     t_complexity = t_complexity + (n-2)
-    print('for one_way_tsp( n =', n,
-          ') Memory complexity:', m_complexity, # (n-2)*2**(n-3) - (n-3)*(n-1)
-          ' Time complexity:', t_complexity )   # (n-2){(n-3)*2**(n-4)-(n-4)}
-    t_complexity = 0
-    m_complexity = 0
-    iset = (n-4)*(n-3) # no of len 3 min routes
-    for i in range(3, n-1):
-        iset = iset // (i-1) * (n-i-2) # no of i+1 len min routes
-        m_complexity = m_complexity + iset
-        t_complexity = t_complexity + iset*(i-1)
-    m_complexity = m_complexity + 1
-    t_complexity = (t_complexity + (n-3))*n*(n-1)//2
-    print('for circular_route_tsp( n =', n, ') Memory complexity:', m_complexity, ' Time complexity:', t_complexity)
-
-points = eval(input())# [(200, 296), (281, 309), (296, 356), (322, 343), (392, 403), (409, 293), (435, 245), (435, 172)]#, (418, 108), (354, 198), (346, 149), (275, 85), (239, 5), (198, 109), (236, 126), (236, 155), (234, 173), (213, 215), (188, 228), (180, 199), (180, 179), (151, 91), (125, 41), (61, 90), (11, 55), (20, 83), (11, 210), (83, 246), (90, 333), (112, 356), (79, 368), (35, 362), (95, 401), (217, 399), (228, 368)]#[(418, 108), (354, 198), (346, 149), (275, 85), (239, 5), (198, 109), (236, 126)]
-
-distance, n, route, complexity = all_dis(points), len(points), {}, 0
-
-#print(get_shortcuts(list(range(1, 1+len(points)))),'shortcuts computed')
-#print(one_way_tsp(1,len(points),[i for i in range(2,len(points))]))
-print(circular_route_tsp(list(range(1, 1+len(points)))))
-print('memory complexity:',len(route.keys()))
+    print('for n =', n,
+          ' Memory complexity:', m_complexity, # (n-2)*2**(n-3) - (n-3)*(n-1)
+          ' Time complexity:', t_complexity )   # (O(n^2.2^n), O(n^3.2^n))                     (n-2){(n-3)*2**(n-4)-(n-4)}
+    
+points = eval(input())
+'''
+Nodes     Time in sec
+13        0.95
+14        2.39
+15        5.95
+16        15.8
+17        33.4
+18        82.2
+19        205.4
+20        448.9
+'''
+distance, n, route, complexity, check = all_dis(points), len(points), dict(), 0, set()
+nodes = {'start':1,len(points):'finish'}
+for i in range(1,len(points)):
+    nodes[i] = i+1
+len3_routes = element_routes(nodes)
+print(circular_route_tsp(nodes))
+print('space complexity:',len(route.keys()))
 print('time complexity:',complexity)
-#complexity_function(7)
